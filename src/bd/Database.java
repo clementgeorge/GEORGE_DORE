92e7d8f11e3fe0930a595ed6fd118b97662d9729
leadore <lea.agnes.dore@gmail.com>
2014-02-27 22:52:14 +0100

    DatabaseTools & AuthentificationTools & Database
    Création de DatabaseTools
    Changement dans AuthentificationTools
    Changement dans Database, formation de multi catch

--- src/bd/Database.java
+++ src/bd/Database.java
@@ -20,43 +20,29 @@ public class Database {
 	   } catch (NamingException e) {
 	      // Handle error that is not configured in JNDI.
 	      throw new SQLException(jndiname + " is missing in JNDI! : "+e.getMessage());
-	   } catch (InstantiationException e) {
+	   } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
-	} catch (IllegalAccessException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (ClassNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+	   } 
 	}
 	
 	public Connection getConnection() throws SQLException {
 	   return (Connection)dataSource.getConnection();
 	}
 
-	
 	public static Connection getMySQLConnection() throws SQLException, ClassNotFoundException {
-
 			try {
 				Class.forName("com.mysql.jdbc.Driver").newInstance();
-			} catch (InstantiationException e) {
+			} catch (InstantiationException | IllegalAccessException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
+			} 	
 		Database database = null;
 	      if (DBStatic.mysql_pooling==false)
-	    	  return (Connection)DriverManager.getConnection("jdbc:mysql://" + DBStatic.mysql_host + "/" +DBStatic.mysql_db, DBStatic.mysql_username,DBStatic.mysql_password);
-	      
+	    	  return (Connection)DriverManager.getConnection("jdbc:mysql://" + DBStatic.mysql_host + "/" +DBStatic.mysql_db, DBStatic.mysql_username,DBStatic.mysql_password);  
 	      else {
 	            if (database == null)
 	                  database = new Database("jdbc/db");
-	            
 	            return(database.getConnection());
 	      }
 	}
